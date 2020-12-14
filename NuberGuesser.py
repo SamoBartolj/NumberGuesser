9085deb0438d9c50bf2cb22899400d1e61160a8e
SamoBartolj <Samo.Bartolj@gmail.com>
2020-12-14 15:29:20 +0100

    Test
    testiru sm

---
+++ NuberGuesser.py
@@ -0,0 +1,34 @@
+
+
+import random
+
+flag = True
+while flag:
+    num = input('Type your nuber for an upper nuber: ')
+
+    if num.isdigit():
+        print("Let's Play! ")
+        num = int(num)
+        flag = False
+
+    else:
+        print('Invalid number!')
+
+secret = random.randint(1,num)
+
+guess = None
+count = 1
+
+while guess != secret:
+    guess = input('Please enter your nuber form 1 to ' + str(num) + ': ')
+
+    if guess.isdigit():
+        guess = int(guess)
+
+    if guess == secret:
+        print('You won!')
+        print('Good Job!')
+
+    else:
+        print('Try again')
+        count += 1
