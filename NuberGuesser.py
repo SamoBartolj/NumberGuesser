650da3a6181773f8f920612aae9b501f5431847e
SamoBartolj <Samo.Bartolj@gmail.com>
2020-12-14 16:08:23 +0100

    Little UpDate

--- NuberGuesser.py
+++ NuberGuesser.py
@@ -1,34 +1,33 @@
-
-
 import random
 
 flag = True
-while flag:
-    num = input('Type your nuber for an upper nuber: ')
 
-    if num.isdigit():
-        print("Let's Play! ")
+while flag:
+    num = input('Plese insert your highiest number: ')
+
+    if num.isdigit:
+        print("Let's Play!")
         num = int(num)
         flag = False
 
     else:
-        print('Invalid number!')
+        print('Invalid number')
+
 
 secret = random.randint(1,num)
 
 guess = None
-count = 1
+count = 0
 
 while guess != secret:
-    guess = input('Please enter your nuber form 1 to ' + str(num) + ': ')
+    guess = input('Please enter your number form 1 to ' + str(num) + ': ')
 
-    if guess.isdigit():
+    if guess.isdigit:
         guess = int(guess)
 
     if guess == secret:
         print('You won!')
-       
 
     else:
-        print('Try again')
+        print('Try again!')
         count += 1
